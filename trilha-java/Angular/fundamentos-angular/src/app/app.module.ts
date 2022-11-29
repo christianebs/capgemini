cf6e1d0039dfd2c66f80494120f136e32fc630cc
Christiane <christianebs90@gmail.com>
2022-11-28 22:06:21 -0300

    exercicios angular

--- trilha-java/Angular/fundamentos-angular/src/app/app.module.ts
+++ trilha-java/Angular/fundamentos-angular/src/app/app.module.ts
@@ -15,6 +15,8 @@ import { ComponentePaiComponent } from './componente-pai/componente-pai.componen
 import { ComponenteFilhoComponent } from './componente-filho/componente-filho.component';
 import { ExemploServicos1Component } from './exemplo-servicos1/exemplo-servicos1.component';
 import { ExemploServicos2Component } from './exemplo-servicos2/exemplo-servicos2.component';
+import { CicloDeVidaComponent } from './ciclo-de-vida/ciclo-de-vida.component';
+import { CicloDeVidaPaiComponent } from './ciclo-de-vida-pai/ciclo-de-vida-pai.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +31,9 @@ import { ExemploServicos2Component } from './exemplo-servicos2/exemplo-servicos2
     ComponentePaiComponent,
     ComponenteFilhoComponent,
     ExemploServicos1Component,
-    ExemploServicos2Component
+    ExemploServicos2Component,
+    CicloDeVidaComponent,
+    CicloDeVidaPaiComponent
   ],
   imports: [
     BrowserModule,
