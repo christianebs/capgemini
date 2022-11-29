a6972312f1fc48883f49b5a70e1680fa100b71a8
Christiane <christianebs90@gmail.com>
2022-11-28 23:53:46 -0300

    exercicios angular

--- trilha-java/Angular/roteamento/src/app/app-routing.module.ts
+++ trilha-java/Angular/roteamento/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: "primeira-pagina", component: PrimeiraPaginaComponent },
   { path: "segunda-pagina", component: SegundaPaginaComponent },
   { path: "", redirectTo: "primeira-pagina", pathMatch: "full" },
+  { path: "pagina-com-parametros/:id", component: PaginaComParametrosComponent },
   { path: "**", component: PaginaNaoEncontradaComponent }
 ]
 
--- trilha-java/Angular/roteamento/src/app/app.module.ts
+++ trilha-java/Angular/roteamento/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimeiraPaginaComponent,
     SegundaPaginaComponent,
-    PaginaNaoEncontradaComponent
+    PaginaNaoEncontradaComponent,
+    PaginaComParametrosComponent
   ],
   imports: [
     BrowserModule,
