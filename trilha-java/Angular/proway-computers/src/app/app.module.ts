fdab9772c7e9f42ab2c356e48cc503dad8fb1e89
Christiane <christianebs90@gmail.com>
2022-11-30 16:47:33 -0300

    shop proway computers

--- trilha-java/Angular/proway-computers/src/app/app.module.ts
+++ trilha-java/Angular/proway-computers/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NaoEncontradaComponent } from './nao-encontrada/nao-encontrada.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from "@angular/material/snack-bar"
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { NaoEncontradaComponent } from './nao-encontrada/nao-encontrada.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
