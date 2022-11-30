e60a3c4b1703ffb15c70933244acac19c95e13ec
Christiane <christianebs90@gmail.com>
2022-11-30 19:07:02 -0300

    shop proway computers

--- trilha-java/Angular/proway-computers/src/app/app-routing.module.ts
+++ trilha-java/Angular/proway-computers/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
   { path: "", redirectTo: "produtos", pathMatch: "full" },
-  { path: 'carrinho', loadChildren: () => import('./carrinho/carrinho.module').then(m => m.CarrinhoModule) }
+  { path: 'carrinho', loadChildren: () => import('./carrinho/carrinho.module').then(m => m.CarrinhoModule) },
+  { path: 'contato', loadChildren: () => import('./contato/contato.module').then(m => m.ContatoModule) }
 ];
 
 @NgModule({
